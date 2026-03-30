4de03f2f4fcb6c5b70beb6b66975c06824eada0b
naveenmaroju87 <113169249+naveenmaroju87@users.noreply.github.com>
2026-03-30 17:54:35 +0100

    Update Streamlit_app.py

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -37,16 +37,16 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT
 
 #convert to snowflake dataframe to a pandas dataframe 
 pd_df = my_dataframe.to_pandas()
-st.dataframe(pd_df)
-st.stop()
+#st.dataframe(pd_df)
+#st.stop()
 
 #st.dataframe(data=my_dataframe, use_container_width=True)
 #st.stop()
 
 ingredients_list = st.multiselect(
     "Choose up to 5 ingredients:"
-     ,fruit_list
-    #,my_dataframe
+     #,fruit_list
+    ,my_dataframe
     ,max_selections=5
 )
 
