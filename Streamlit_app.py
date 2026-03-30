5f978fb4ce5a60b46ff3bcea3f9be5c07a53563c
naveenmaroju87 <113169249+naveenmaroju87@users.noreply.github.com>
2026-03-30 16:47:00 +0100

    Update Streamlit_app.py

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -6,7 +6,8 @@ import toml
 
 import requests  
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response.json())
+#st.text(smoothiefroot_response.json())
+sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
 # Load Snowflake config
 config = toml.load("config.toml")
