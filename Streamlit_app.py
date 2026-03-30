717e404f75510a02e773d1d44e33d14e2e5fd6ff
naveenmaroju87 <113169249+naveenmaroju87@users.noreply.github.com>
2026-03-30 17:12:17 +0100

    Update Streamlit_app.py

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -31,11 +31,13 @@ st.write('The name on your Smoothie will be:', name_on_order)
 
 
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
+fruit_list = my_dataframe.to_pandas()['FRUIT_NAME'].tolist()
 #st.dataframe(data=my_dataframe, use_container_width=True)
 
 ingredients_list = st.multiselect(
     "Choose up to 5 ingredients:"
-    ,my_dataframe
+     ,fruit_list
+    #,my_dataframe
     ,max_selections=5
 )
 
@@ -44,10 +46,18 @@ if ingredients_list:
 
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
+        # Use the selected fruit in the API call
+        fruit_api_name = fruit_chosen.lower()
+        url = f"https://my.smoothiefroot.com/api/fruit/{fruit_api_name}"
+
+        smoothiefroot_response = requests.get(url)
+      
         st.subheader(fruit_chosen + ' Nutrition Information')
-        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+      
+        #smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
         #st.text(smoothiefroot_response.json())
         #sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+      
         st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
     #st.write(ingredients_string)
